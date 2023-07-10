d91dfe92c17bd166970796439f7e3400765c747e
Darrenf040 <darrenfr83@gmail.com>
2023-07-10 00:52:58 -0500

    function to play one round of rock paper scissors. (user input is case sensitive)

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 function getComputerChoice() {
-    const arr = ["Rock", "Paper", "Scissors"];
+    const arr = ["rock", "paper", "scissors"];
     return arr[Math.floor(Math.random() * 3)];  //random number from 0-2
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+let computerSelect = getComputerChoice();
+let playerSelect = prompt("Enter Choice");
+
+
+function oneRound(playerSelection, computerSelection){
+
+    if(playerSelection == computerSelection){
+        return console.log("Tie")
+    }
+    else if((playerSelection == "rock" && computerSelection == "scissors")){
+        return console.log("You Win");
+    }
+    else if((playerSelection == "rock" && computerSelection == "paper")){
+        return console.log("You Lose");
+    }
+    else if((playerSelection == "paper" && computerSelection == "rock")){
+        return console.log("You Win");
+    }
+    else if((playerSelection == "paper" && computerSelection == "scissors")){
+        return console.log("You Lose");
+    }
+    else if((playerSelection == "scissors" && computerSelection == "rock")){
+        return console.log("You Lose");
+    }
+    else if((playerSelection == "scissors" && computerSelection == "paper")){
+        return console.log("You Win");
+    }
+}
+console.log(oneRound(computerSelect, playerSelect))
